be9ed0dcb3e612eccae07e2df8831757aa1be36c
Escartem <escartem.yt@gmail.com>
2021-11-25 20:17:15 +0100

    initial commit

---
+++ app/Frame/Frame/App.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class App
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(App))
+        Me.bg = New System.Windows.Forms.PictureBox()
+        CType(Me.bg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'bg
+        '
+        Me.bg.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.bg.Image = CType(resources.GetObject("bg.Image"), System.Drawing.Image)
+        Me.bg.Location = New System.Drawing.Point(12, 12)
+        Me.bg.Name = "bg"
+        Me.bg.Size = New System.Drawing.Size(776, 426)
+        Me.bg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.bg.TabIndex = 0
+        Me.bg.TabStop = False
+        '
+        'App
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.bg)
+        Me.Name = "App"
+        Me.Text = "Frame"
+        Me.TopMost = True
+        CType(Me.bg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents bg As PictureBox
+End Class
---
+++ app/Frame/Frame/App.vb
@@ -0,0 +1,36 @@
+﻿Public Class App
+    Private Sub App_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' rounded corners
+        ' roundCorners(Me)
+        ' transition
+        ' Me.Opacity = 0
+        ' Dim i As Integer
+        ' For i = 1 To 100 Step +1
+        ' Me.Opacity = i / 100
+        ' Me.Refresh()
+        ' Threading.Thread.Sleep(10)
+        ' Next
+    End Sub
+
+    Private Sub roundCorners(obj As Form)
+        obj.FormBorderStyle = FormBorderStyle.None
+
+        Dim DGP As New Drawing2D.GraphicsPath
+        DGP.StartFigure()
+
+        ' Top Left
+        DGP.AddArc(New Rectangle(0, 0, 40, 40), 180, 90)
+        DGP.AddLine(40, 0, obj.Width - 40, 0)
+
+        ' Top Right
+        DGP.AddArc(New Rectangle(obj.Width - 40, 0, 40, 40), -90, 90)
+        DGP.AddLine(obj.Width, 40, obj.Width, obj.Height - 40)
+
+        ' Bottom Right
+        DGP.AddArc(New Rectangle(obj.Width - 40, obj.Height - 40, 40, 40), 0, 90)
+        DGP.AddLine(obj.Width - 40, obj.Height, 40, obj.Height)
+
+        obj.Region = New Region(DGP)
+    End Sub
+
+End Class
